57d18036cb32659be2390c100b7aa28135746543
Olexander <ssolexa@gmail.com>
2020-07-16 08:49:55 +0300

    add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
-import Dialogs from './components/dialogs/Dialogs';
 import Profile from './components/profile/Profile';
+import Dialogs from './components/dialogs/Dialogs';
+import News from './components/news/News';
+import Music from './components/music/Music';
+import Settings from './components/settings/Settings';
+import {BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 
 const App = () => {
-  
+
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app__content'>
-        <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app__content'>
+          <Route path='/profile' component={Profile} />
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+        </div>
       </div>
-      {/* <Profile /> */}
-    </div>
+    </BrowserRouter>
   )
-  
+
 }
 
 export default App;
