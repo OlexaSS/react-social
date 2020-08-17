749cf027c3dd08e7343378e9fc5d7a0b92c71123
Olexander <ssolexa@gmail.com>
2020-08-17 17:24:58 +0300

    life cikle didMount

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -5,14 +5,15 @@ import userPhoto from '../../accets/images/user.jpg';
 
 
 class Users extends React.Component {
-
-    constructor(props){
-        super(props);
+    componentDidMount(){
+        if (this.props.users.length === 0){
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
 
-                this.props.setUsers(response.data.items);
-            });
+            this.props.setUsers(response.data.items);
+        });
     }
+    }
+
     render() {
         return <div>
             {this.props.users.map(u => <div key={u.id}>
